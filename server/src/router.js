1537b157c59b9f2f6a4832f5fcdd6395aed3faa6
UlviHuseynzade <ulviyh@code.edu.az>
2021-06-16 12:13:01 +0400

    initial commit

---
+++ server/src/router.js
@@ -0,0 +1,7 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationContollerPolicy')
+
+module.exports = (app) => {
+    app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register),
+    app.post('/login', AuthenticationController.login)
+}
